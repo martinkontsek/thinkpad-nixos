743a645f5d1ac43e7ef80118a28dcf5cd80d8ff8
Martin Kontsek <martinkontsek@gmail.com>
2025-11-06 08:01:09 +0100

    Config app associations via home manager.

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -4,7 +4,28 @@
     mime.enable = true;
     mimeApps = {
       enable = true;
+      associations.added = {
+        "application/pdf" = ["org.gnome.Evince.desktop"];
+        "image/jpeg" = ["org.gnome.Loupe.desktop"];
+        "image/png" = ["org.gnome.Loupe.desktop"];
+        "audio/mpeg" = ["org.gnome.Rhythmbox3.desktop"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "text/html" = ["firefox.desktop"];
+        "text/plain" = ["org.gnome.gedit.desktop"];
+      };
+      defaultApplications = {
+        "application/pdf" = ["org.gnome.Evince.desktop"];
+        "image/jpeg" = ["org.gnome.Loupe.desktop"];
+        "image/png" = ["org.gnome.Loupe.desktop"];
+        "audio/mpeg" = ["org.gnome.Rhythmbox3.desktop"];
+        "x-scheme-handler/http" = ["firefox.desktop"];
+        "x-scheme-handler/https" = ["firefox.desktop"];
+        "text/html" = ["firefox.desktop"];
+        "text/plain" = ["org.gnome.gedit.desktop"];
+      };
     };
+    configFile."mimeapps.list".force = true;
     portal = {
       enable = true;
       extraPortals = [pkgs.xdg-desktop-portal-hyprland];
