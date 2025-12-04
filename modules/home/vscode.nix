ac87752e3328dabfcedd580d0c74c1aaf207d8a9
Martin Kontsek <martinkontsek@gmail.com>
2025-12-04 20:50:00 +0100

    Add vscode plugins.

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -8,13 +8,24 @@
         extensions = with pkgs.vscode-extensions; [
           bbenoist.nix
           jeff-hykin.better-nix-syntax
+          ms-vscode.cpptools
           ms-vscode.cpptools-extension-pack
           #vscodevim.vim # Vim emulation
           mads-hartmann.bash-ide-vscode
           zainchen.json
           ms-python.python
+          ms-python.vscode-pylance
+          ms-python.debugpy
+          ms-vscode-remote.remote-ssh-edit
           ms-vscode-remote.remote-ssh
           ms-vscode.remote-explorer
+          ms-azuretools.vscode-containers
+          eamodio.gitlens
+          adpyke.codesnap
+          redhat.ansible
+          moshfeu.compare-folders
+          golang.go
+          redhat.vscode-yaml
         ];
         userSettings = {
           "workbench.colorTheme" = "Visual Studio Dark";
