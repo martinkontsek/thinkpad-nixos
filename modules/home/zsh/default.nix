40ebe057aa4847eeea033a649f61539f545496d5
Martin Kontsek <martinkontsek@gmail.com>
2025-12-04 20:49:37 +0100

    Add fi alias.

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -54,6 +54,7 @@
       sv = "sudo nvim";
       v = "nvim";
       c = "clear";
+      fi = "nh os info";
       fr = "nh os switch --hostname ${profile}";
       fu = "nh os switch --hostname ${profile} --update";
       zu = "sh <(curl -L https://gitlab.com/Zaney/zaneyos/-/releases/latest/download/install-zaneyos.sh)";
